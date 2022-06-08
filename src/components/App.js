4c297aaf987fa551059e137f51b8ddfaf3112dd3
Rachel Bright <rachel.bright@checkr.com>
2022-06-07 22:04:37 -0700

    Add functionality to leaderboard page

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./Dashboard";
 import LoadingBar from "react-redux-loading-bar";
 import PollPage from "./PollPage";
 import NewPoll from "./NewPoll";
+import Leaderboard from "./Leaderboard";
 
 const App = (props) => {
   useEffect(() => {
@@ -16,7 +17,8 @@ const App = (props) => {
       {/* {props.loading === true ? null : <Dashboard />} */}
       {/* {props.loading === true ? null : <PollPage match={{ params: { id: "vthrdm985a262al8qx3do" } }} />} */}
       {/* {props.loading === true ? null : <PollPage match={{ params: { id: "loxhs1bqm25b708cmbf3g" } }} />} */}
-      {props.loading === true ? null : <NewPoll />}
+      {/* {props.loading === true ? null : <NewPoll />} */}
+      {props.loading === true ? null : <Leaderboard />}
     </div>
   );
 };
---
+++ src/components/Leaderboard.js
@@ -0,0 +1,68 @@
+import { connect } from "react-redux";
+import {
+  MDBTable,
+  MDBTableHead,
+  MDBTableBody,
+  MDBCol,
+  MDBRow,
+} from "mdb-react-ui-kit";
+
+const Leaderboard = ({ users }) => {
+  let usersInfo = Object.values(users);
+  usersInfo.map(
+    (user) =>
+      (user.score = user.questions.length + Object.keys(user.answers).length)
+  );
+  const sortedUsers = usersInfo.sort((a, b) => b.score - a.score);
+
+  return (
+    <div className="mb-4 d-flex align-items-center justify-content-center">
+      <MDBTable style={{ width: "48rem" }}>
+        <MDBTableHead light>
+          <tr>
+            <th scope="col">Users</th>
+            <th scope="col">Answered</th>
+            <th scope="col">Created</th>
+            <th scope="col">Score</th>
+          </tr>
+        </MDBTableHead>
+        <MDBTableBody>
+          {sortedUsers.map((user) => {
+            return (
+              <tr key={user.id}>
+                <td>
+                  <MDBRow>
+                    {/* TODO: Try to fix alignment of avatar and user name */}
+                    <MDBCol style={{ width: "0rem" }}>
+                      <img
+                        alt="avatar"
+                        className="avatar"
+                        src={`${user.avatarURL}`}
+                      />
+                    </MDBCol>
+                    <MDBCol>
+                      <span>{user.name}</span>
+                      <br></br>
+                      <span className="text-muted">{user.id}</span>
+                    </MDBCol>
+                  </MDBRow>
+                </td>
+                <td>{Object.keys(user.answers).length}</td>
+                <td>{user.questions.length}</td>
+                <td>{user.score}</td>
+              </tr>
+            );
+          })}
+        </MDBTableBody>
+      </MDBTable>
+    </div>
+  );
+};
+
+function mapStateToProps({ users }) {
+  return {
+    users,
+  };
+}
+
+export default connect(mapStateToProps)(Leaderboard);
