4751a489fffa6cad6aff81aec2540250e9b3973d
Rachel Bright <rachel.bright@checkr.com>
2022-06-10 10:48:47 -0700

    Clean up tests

--- src/tests/LoginPage.test.js
+++ src/tests/LoginPage.test.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import "@testing-library/jest-dom";
 import { render, fireEvent, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
